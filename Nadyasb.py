ad3cf9f8b502d84ce0ab2abb9b2b97216bafeba9
luthfimhmmd <32026634+luthfimhmmd@users.noreply.github.com>
2018-12-08 17:19:04 +0700

    Update Nadyasb.py

--- Nadyasb.py
+++ Nadyasb.py
@@ -76,7 +76,7 @@ def sendMessageWithMention(to, mid):
         
 def helpmessage():
     helpMessage = "╔═══════════════" + "\n" + \
-                  "╠♥ ✿✿✿ mr musa ✿✿✿ ♥" + "\n" + \
+                  "╠ ✿✿✿ WELCOME ✿✿✿ " + "\n" + \
                   "║" + "\n" + \
                   "╠══✪〘 Help Message 〙✪══" + "\n" + \
                   "║" + "\n" + \
@@ -151,7 +151,7 @@ def helpmessage():
                   "╠➥ SearchImage「Search」" + "\n" + \
                   "╠➥ ScreenshootWebsite「LinkUrl」" + "\n" + \
                   "║" + "\n" + \
-                  "╚═〘 Credits By: ©MUSA  〙"
+                  "╚═〘 Credits By: Luthfi  〙"
     return helpMessage
     
 def helptexttospeech():
@@ -210,7 +210,7 @@ def helptexttospeech():
                          "╠ vi : Vietnamese" + "\n" + \
                          "╠ cy : Welsh" + "\n" + \
                          "╚══[ Jangan Typo ]" + "\n" + "\n\n" + \
-                          "Contoh : say-id Nadya Cantik"
+                          "Contoh : say-id Chaca Cantik"
     return helpTextToSpeech
     
 def helptranslate():
@@ -322,7 +322,7 @@ def helptranslate():
                        "╠ fil : Filipino" + "\n" + \
                        "╠ he : Hebrew" + "\n" + \
                        "╚══[ Jangan Typo ]" + "\n" + "\n\n" + \
-                         "Contoh : tr-id Nadya Cantik"
+                         "Contoh : tr-id chaca Cantik"
     return helpTranslate
 #==============================================================================#
 def lineBot(op):
